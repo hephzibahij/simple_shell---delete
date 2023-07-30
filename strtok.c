02272d777523b65ac781c7dbd5e3ce0361860437
Hephzibah <hephzibahijeje89@gmail.com>
2023-07-30 15:20:55 -0700

    strtok.c

---
+++ strtok.c
@@ -0,0 +1,68 @@
+#include "shell.h"
+
+/**
+ * delimiter - function that checks if a character matchs any character
+ * check_delim - functions to be named
+ * @c: character to check
+ * @str: string of delimiters
+ * Return: 1 on success, 0 on failure
+ */
+size_t delimiter(char c, const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (c == str[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * _strtok - function that extracts tokens of a string
+ * @str: string
+ * @delim: delimiter
+ * Return: pointer to the next token or NULL
+ */
+char *_strtok(char *str, const char *delim)
+{
+	static char *tokens;
+	static char *new_token;
+	unsigned int i;
+
+	if (str != NULL)
+		new_token = str;
+	tokens = new_token;
+	if (tokens == NULL)
+		return (NULL);
+	for (i = 0; tokens[i] != '\0'; i++)
+	{
+		if (check_delim(tokens[i], delim) == 0)
+			break;
+	}
+	if (new_token[i] == '\0' || new_token[i] == '#')
+	{
+		new_token = NULL;
+		return (NULL);
+	}
+	tokens = new_token + i;
+	new_token = tokens;
+	for (i = 0; new_token[i] != '\0'; i++)
+	{
+		if (delimiter(new_token[i], delim) == 1)
+			break;
+	}
+	if (new_token[i] == '\0')
+		new_token = NULL;
+	else
+	{
+		new_token[i] = '\0';
+		new_token = new_token + i + 1;
+		if (*new_token == '\0')
+			new_token = NULL;
+	}
+	return (tokens);
+}
